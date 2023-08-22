5c2e347f23feed0dc505496642562da1a384abf7
Gabriele Cannata <Canna71@users.noreply.github.com>
2023-08-22 18:34:05 +0200

    Editing filename

--- src/Views/components/FileName.tsx
+++ src/Views/components/FileName.tsx
@@ -5,17 +5,28 @@ import { ObjectData } from "src/Data/ObjectData";
 import { useApp } from "./AppProvider";
 
 const FileName: Component<{ data: ObjectData; attribute: AttributeDefinition; }> = (props) => {
-    const app = useApp();
+    const app = useApp()!;
     const text = () => props.attribute.format(props.data);
     const [isEdit, setEdit] = createSignal(false);
-    let editor;
+    let editor : HTMLDivElement;
+
+    console.log("Rendering FileName")
 
     const onEdit = (e: MouseEvent) => {
         if (!isEdit()) setEdit(true);
     };
 
-    const exitEdit = () => {
+    const renameFile = (name: string) => {
+        const file = props.data.file;
+        //@ts-ignore
+        const newName = file.getNewPathAfterRename(name);
+        app.fileManager.renameFile(file, newName);
+        // debugger;
+    }
+
+    const onBlur = () => {
         setEdit(false);
+        renameFile(editor.innerText.trim())
     };
 
     const onClick = async (e:MouseEvent) => {
@@ -28,6 +39,16 @@ const FileName: Component<{ data: ObjectData; attribute: AttributeDefinition; }>
         await leaf?.openFile(file);
     }
 
+    const onInput = (e:InputEvent) => {
+        console.log(e);
+        // TODO: check valid title
+    } 
+
+    const onkeydown = (e:KeyboardEvent) => {
+        console.log(e);
+        // TODO: check valid title
+    } 
+
     return <>
         <Show when={!isEdit()}>
             <div class="sets-cell-filename">
@@ -36,7 +57,14 @@ const FileName: Component<{ data: ObjectData; attribute: AttributeDefinition; }>
             </div>
         </Show>
         <Show when={isEdit()}>
-            <div ref={editor} class="sets-cell-filename">
+            <div ref={editor!} class="sets-cell-filename" contentEditable={true} 
+                autocapitalize="on"
+                
+                spellcheck={app.vault.getConfig("spellcheck")}
+                onInput={onInput} 
+                onBlur={onBlur}
+                onKeyDown={onkeydown}
+                >{text()}
                 
             </div>
         </Show>
