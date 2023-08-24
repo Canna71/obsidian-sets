436d52cb7952a142d4aff5191e9152fa07eeba76
Gabriele Cannata <gcannata@gmail.com>
2023-08-24 21:29:08 +0200

    drag drop POX

--- src/Views/components/Header.tsx
+++ src/Views/components/Header.tsx
@@ -4,17 +4,17 @@ import { useGrid } from "./GridProvider";
 export const Header: Component<{ name: string; key: string; }> = (props) => {
     const gridContext = useGrid();
 
-    const {state, onHover,onExit} = gridContext!;
+    const { state, onHover, onExit } = gridContext!;
 
     const onMouseOver = (e) => {
-        if(gridContext !== undefined){
-            
+        if (gridContext !== undefined) {
+
             onHover(props.key);
         }
     }
 
     const onMouseLeave = (e) => {
-        if(gridContext !== undefined){
+        if (gridContext !== undefined) {
             onExit();
         }
     }
@@ -22,12 +22,36 @@ export const Header: Component<{ name: string; key: string; }> = (props) => {
     const isHovering = () => {
         return state?.().hovering === props.key;
     }
+    const drag = (e:DragEvent) => {
+        e.dataTransfer?.setData("text", props.key);
+    }
 
-    return (<div class="sets-header-cell" 
+    const allowDrop = (e:DragEvent) => {
+        
+        const data = e.dataTransfer?.getData("text");
+        console.log(`allowDrop`, data);
+        if(data !== props.key){
+            e.preventDefault();
+        }
+    }
+
+    const drop = (e:DragEvent) => {
+        const data = e.dataTransfer?.getData("text");
+        console.log(`drop`, data);
+        e.preventDefault();
+    }
+
+    return (<div class="sets-header-cell"
+        draggable={true}
         classList={{
             hovered: isHovering()
         }}
-    onmouseover={onMouseOver} onmouseleave={onMouseLeave}>
+        ondragover={allowDrop}
+        ondragstart={drag}
+        onmouseover={onMouseOver} 
+        onmouseleave={onMouseLeave}
+        ondrop={drop}
+        >
         <div class="sets-cell-content">
             <div>{props.name}</div>
             <div class="sets-column-resizer"></div>
