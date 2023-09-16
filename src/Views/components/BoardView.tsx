435e96f751ee6e72c321b5d07dd068910ab1dfde
Gabriele Cannata <gcannata@gmail.com>
2023-09-16 22:29:44 +0200

    Using editable fields in Board

--- src/Views/components/BoardView.tsx
+++ src/Views/components/BoardView.tsx
@@ -1,9 +1,11 @@
-import { Component, For, createMemo } from "solid-js";
+import { Component, For, Show, createMemo } from "solid-js";
 import { SetViewProps } from "./GridView";
 import { useBlock } from "./SetProvider";
 import { groupBy } from "src/Utils/groupBy";
 import { useApp } from "./AppProvider";
 import AttributeView from "./AttributeView";
+import { EditProp } from "./EditProp";
+import FileName from "./FileName";
 
 const BoardView: Component<SetViewProps> = (props) => {
     const { definition } = useBlock()!;
@@ -36,11 +38,25 @@ const BoardView: Component<SetViewProps> = (props) => {
                             {lane}
                         </div>
 
-                        <For each={groupedData()[lane]}>{(item, i) =>
+                        <For each={groupedData()[lane]}>{(data, i) =>
                             <div class="sets-board-item">
                                 <For each={props.attributes}>{(attribute, i) =>
-                                    <div class="sets-board-item-field">
-                                        <AttributeView attribute={attribute} data={item} />
+                                    <div class="sets-board-item-field" title={attribute.displayName()}>
+                                        {/* <AttributeView attribute={attribute} data={item} /> */}
+                                        <Show when={attribute.readonly}>
+                                            <div class="sets-cell-read">{attribute.format(data)}</div>
+                                        </Show>
+                                        <Show when={!attribute.isIntrinsic}>
+
+                                            <div class="sets-view-field">
+                                                <EditProp data={data} attribute={attribute} />
+                                            </div>
+                                        </Show>
+                                        <Show when={attribute.isIntrinsic && !attribute.readonly}>
+                                            <div class="sets-view-field">
+                                                <FileName data={data} attribute={attribute} />
+                                            </div>
+                                        </Show>
                                     </div>
                                 }</For>
                             </div>
