e7320d9f1fdda301abfab47250224f068b56990e
Gabriele Cannata <gcannata@gmail.com>
2023-09-17 18:31:22 +0200

    Grab Handle in Board

--- src/Views/components/BoardItem.tsx
+++ src/Views/components/BoardItem.tsx
@@ -1,14 +1,24 @@
-import { Component } from "solid-js";
+import { Component, onMount } from "solid-js";
 import { createDraggable } from "@thisbeyond/solid-dnd";
 import { CardItem } from "./CardItem";
 import { ItemProps } from "./CardItem";
+import { setIcon } from "obsidian";
 
 
 export const BoardItem: Component<ItemProps> = (props) => {
     const draggable = createDraggable(props.data.file.path, props.data);
 
+    let grabHandle: HTMLDivElement | null = null;
+
+    onMount(() => {
+        if(grabHandle) setIcon(grabHandle, "grip-horizontal");
+    });
+
     return (
-        <div class="sets-board-item" use: draggable>
+        <div class="sets-board-item" use: draggable >
+            <div ref={grabHandle!} class="sets-board-item-handle">
+
+            </div>
             <CardItem {...props} />
         </div>
     );
